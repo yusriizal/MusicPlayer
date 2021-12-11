e52efbad6683a210de8a1a9f4758caf7ea1c91b5
Yusriizal <61347964+yusriizal@users.noreply.github.com>
2021-12-12 01:45:49 +0700

    Add files via upload

---
+++ app/src/main/java/com/android/example/musicplayer/MainAdapter.kt
@@ -0,0 +1,47 @@
+package com.android.example.musicplayer
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.android.example.musicplayer.R
+import com.android.example.musicplayer.PlaySongActivity
+import com.android.example.musicplayer.MainAdapter.MyViewHolder
+import kotlin.android.synthetic.main.list_item_main.view.*
+import java.util.*
+
+class MainAdapter(var songList: ArrayList<HashMap<String, String>>, var context: Context) : RecyclerView.Adapter<MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_main, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.textView.text = songList[position]["songTitle"].replace("_", " ")
+        holder.cvListMusic.setOnClickListener {
+            val intent = Intent(context, PlaySongActivity::class.java)
+            intent.putExtra("songIndex", songList[position])
+            context.startActivity(intent)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return songList.size
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var cvListMusic: CardView
+        var textView: TextView
+
+        init {
+            cvListMusic = itemView.cvListMusic
+            textView = itemView.tvJudulLagu
+        }
+    }
+
+}
---
+++ app/src/main/java/com/android/example/musicplayer/SongsManager.kt
@@ -0,0 +1,32 @@
+package com.android.example.musicplayer
+
+import android.os.Environment
+import java.io.File
+import java.io.FilenameFilter
+import java.util.*
+
+class SongsManager {
+
+    var MEDIA_PATH = Environment.getExternalStorageDirectory().toString()
+    private val songsList = ArrayList<HashMap<String, String>>()
+
+    fun getPlayList(): ArrayList<HashMap<String, String>> {
+        val home = File(MEDIA_PATH)
+        if (home.listFiles(FileExtensionFilter())) {
+            for (file in home.listFiles(FileExtensionFilter())) {
+                val song = HashMap<String, String>()
+                song["songTitle"] = file.name.substring(0, file.name.length - 4)
+                song["songPath"] = file.path
+                songsList.add(song)
+            }
+        }
+        return songsList
+    }
+
+    internal inner class FileExtensionFilter : FilenameFilter {
+        override fun accept(dir: File, name: String): Boolean {
+            return name.endsWith(".mp3") || name.endsWith(".MP3")
+        }
+    }
+
+}
